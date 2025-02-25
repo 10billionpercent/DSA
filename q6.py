a4548fc7db3d6db4e670d26c6a5a336ca9bf9aa9
hollow-purple12 <103302612+hollow-purple12@users.noreply.github.com>
2025-02-25 09:51:03 +0530

    Update q6.py

--- q6.py
+++ q6.py
@@ -1,3 +1,4 @@
+"""You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers. You should return the array of nums such that the the array follows the given conditions: . Every consecutive pair of integers have opposite signs."""
 def show(n,arr):
     p=[]
     n_=[]
