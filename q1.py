168dea9a857d524ac7958e7ddb5ef2afe4797142
hollow-purple12 <103302612+hollow-purple12@users.noreply.github.com>
2025-02-25 09:46:32 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,3 +1,6 @@
+"""You are given an array of integers and a target integer k. Write a function to find the element in the array that is closest to k. If there are multiple elements equidistant from k, return the smallest of them."""
+
+
 import math
 n=int(input())
 arr=list(map(int,input().split()))
