8cfbdd07958d78c281c33e87d4a796f87007bec3
10billionpercent <103302612+10billionpercent@users.noreply.github.com>
2025-05-28 11:52:50 +0530

    Update 04_BnB_TSP.py

--- 04_BnB_TSP.py
+++ 04_BnB_TSP.py
@@ -1,13 +1,29 @@
-from heapq import*
-n=int(input())
-d=[list(map(int,input().split()))for _ in range(n)]
-q=[(0,0,[0],set([0]))]
-best=1e9
-while q:
- c,u,path,vis=heappop(q)
- if len(path)==n:
-  best=min(best,c+d[u][0]);continue
- for v in range(n):
-  if v not in vis:
-   heappush(q,(c+d[u][v],v,path+[v],vis|{v}))
-print(best)
+def tsp_bb(cost):
+    n = len(cost)
+    best = [float('inf')]
+
+    def dfs(path, vis, curr, cost_so_far):
+        if len(path) == n:
+            best[0] = min(best[0], cost_so_far + cost[curr][0])
+            return
+        if cost_so_far >= best[0]:
+            return
+        for i in range(n):
+            if not vis[i]:
+                vis[i] = 1
+                dfs(path + [i], vis, i, cost_so_far + cost[curr][i])
+                vis[i] = 0
+
+    vis = [0] * n
+    vis[0] = 1
+    dfs([0], vis, 0, 0)
+    return best[0]
+
+cost = [
+    [0, 10, 15, 20],
+    [10, 0, 35, 25],
+    [15, 35, 0, 30],
+    [20, 25, 30, 0]
+]
+
+print("TSP Branch and Bound:", tsp_bb(cost))
